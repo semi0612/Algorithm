6228d04832a442dd99fdc341bc4140a281ce7edb
Choi semi <cyhse7@gmail.com>
2021-04-06 21:21:24 +0900

    Create 11654 : 아스키 코드.java

---
+++ BOJ/Java/11654 : 아스키 코드.java
@@ -0,0 +1,9 @@
+// 풀이 (https://velog.io/@cyhse7/%EC%9E%90%EB%B0%94-%EB%B0%B1%EC%A4%80-11654-%EC%95%84%EC%8A%A4%ED%82%A4-%EC%BD%94%EB%93%9C)
+
+// 사용자에게 입력 받기
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// String에서 cahr로 변환하는데
+// 그걸 int 타입으로 저장하면서 문자에 대응되는 값으로 저장된다
+int input = br.readLine().charAt(0);
+		
+System.out.println(input);
